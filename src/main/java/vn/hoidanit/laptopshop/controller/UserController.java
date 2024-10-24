d653fbecc3a0f8ef69d01451e472b4cda2158475
locnht-it <locnht.it@gmail.com>
2024-10-24 22:11:31 +0700

    video 52

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -29,6 +29,11 @@ public class UserController {
         model.addAttribute("it", "from controller with model");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        return "admin/user/create";
+    }
 }
 
 // @RestController
